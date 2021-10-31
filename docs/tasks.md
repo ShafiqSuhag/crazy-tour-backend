c6b75f7f4de7281941acd7af17250a4d262d944e
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-10-31 22:32:32 +0600

    final update

--- docs/tasks.md
+++ docs/tasks.md
@@ -11,15 +11,15 @@ We have a sample task for you.
 
 ## Task Detail: 
 [done] 1. Home page will have a header (simple navbar), banner, offerings (services/items/options/plans:  the main items that user will purchase), and footer.
- 2. Add two extra sections in the home page in addition to the 4 sections mentioned above. 
+ [done]  2. Add two extra sections in the home page in addition to the 4 sections mentioned above. 
 
 [done] 3. The Offerings will have at least 6 items/services. You can put one or more items/services in a row based on your design. Each service should have a relevant name, image, short description and a purchase button (button could say: book now, buy now, order, etc.). 
-# 4. Clicking on the button will take the user to the place order page. This route will be a private/protected route. Please make sure that if the user is not logged in, the private route redirects to the login page. After login, the user will be redirected to the page he/she wanted to go to. Also, make sure, after reloading the page of a private route, the user is not redirected to the login page.
+[done]  4. Clicking on the button will take the user to the place order page. This route will be a private/protected route. Please make sure that if the user is not logged in, the private route redirects to the login page. After login, the user will be redirected to the page he/she wanted to go to. Also, make sure, after reloading the page of a private route, the user is not redirected to the login page.
 [done]  5. The Place Order page should display detailed information of the item, user has clicked. It will display users name and email address from the login system and user will provide address and necessary information to place the order/booking/purchase. 
 
 
 [done] 6. Implement at least one (google, facebook, github,or email/ password) authentication. Once a user is logged in, the user name, logout button should appear on the header which will log out the user once clicked. Displaying user profile pic on the header is optional.
-7. If a user is logged in, he/she will see more options like `My Orders`, `Manage All Orders`, `Add A New Service`, `Logout`. Based on your website idea, you can change the name of these menu items. 
+7. If a user is logged in, he/she will see more options like `My Orders`, `Manage All Orders`, `Add A New Tour Package`, `Logout`. Based on your website idea, you can change the name of these menu items. 
 8. On the my orders page, the logged in user will see only his/her orders. If the user wants, he/she should be able to cancel/delete any order. Ask a confirmation message before deleting or canceling an item. Using browser confirmation dialog is ok. 
 
 
--- index.js
+++ index.js
@@ -86,8 +86,6 @@ async function run() {
           }
         )
       }
-
-
     })
     // find servier 
     app.post('/tour-details', async (req, res) => {
@@ -160,6 +158,141 @@ async function run() {
 
     })
     // delete ./
+
+
+
+    //**************************************************************************** ORDERS  */
+    // collection name 
+    const orderCollection = db.collection("orders");
+    // store 
+    app.post('/my-orders', async (req, res) => {
+      console.log('inside post my-orders ')
+      console.log(req.body, typeof req.body)
+      const doc = req.body || {}
+      // res.send()
+      // return     
+      if (doc) {
+        const result = await orderCollection.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        res.send({
+          success: true,
+          msg: `A document was inserted with the _id: ${result.insertedId}`
+        })
+      } else {
+        res.send(
+          {
+            success: false,
+            msg: `Failed to post`
+          }
+        )
+      }
+    })
+    // ######## 
+    // GET MY ORDERS
+    // ######## 
+    app.get('/my-orders/:id', async (req, res) => {
+      const id = req.params.id
+      console.log('inside find my-orders', id, typeof id)
+      // res.send(id)
+      // return;
+      const limit = parseInt(req.query.size)
+      const pageNumber = parseInt(req.query.page)
+      console.log("limit - pageNumber", limit, pageNumber)
+      // const _id = ObjectId(id);
+      // Query for a movie that has title "Annie Hall"
+      const query = { userId: id };
+      
+      const options = {
+        skip: pageNumber * limit || 0,
+        limit: limit || 0
+      }
+      const count = await orderCollection.estimatedDocumentCount();
+
+      // const count = await cursor.count();
+      const cursor = orderCollection.find(query, options)
+
+      const orderList = await cursor.toArray()
+      if (pageNumber) {
+
+      }
+      else {
+
+      }
+      res.json({
+        count: count,
+        orderList: orderList,
+
+      })
+    })
+    // ######## 
+    // GET ALL ORDERS
+    // ######## 
+    app.get('/all-orders', async (req, res) => {
+
+      const limit = parseInt(req.query.size)
+      const pageNumber = parseInt(req.query.page)
+      console.log("limit - pageNumber", limit, pageNumber)
+      const query = {}
+      const options = {
+        skip: pageNumber * limit || 0,
+        limit: limit || 0
+      }
+      const count = await orderCollection.estimatedDocumentCount();
+
+      // const count = await cursor.count();
+      const cursor = orderCollection.find(query, options)
+
+      const orderList = await cursor.toArray()
+      if (pageNumber) {
+
+      }
+      else {
+
+      }
+      res.json({
+        count: count,
+        orderList: orderList,
+
+      })
+    })
+
+    // delete 
+
+
+    app.delete('/my-orders', async (req, res) => {
+      console.log('inside delete req ')
+      console.log(req.body, typeof req.body)
+      const doc = req.body || {}
+
+      // res.send(doc)
+      // return   
+      const _id = ObjectId(doc.id);
+      // Query for a movie that has title "Annie Hall"
+      const query = { _id: _id };
+      const result = await orderCollection.deleteOne(query);
+      console.log("check delete ",result)
+      if (result.deletedCount === 1) {
+        console.log("delete success")
+        res.send({
+          success: true,
+          msg: "Successfully deleted one document.",
+          id: doc.id
+        })
+
+       
+      } else {
+        res.send({
+          success: false,
+          msg: "Failed to delete",
+          id: doc.id
+        })
+      }
+
+    })
+    // delete ./
+    //**************************************************************************** ORDERS  */
+
+
 
 
   }
